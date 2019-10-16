0f328c1d189a0d3b875c45b648042f742a87d769
Josh Cunningham <josh@joshcanhelp.com>
2019-10-16 13:33:00 -0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.openid.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
