db7e13e51e3b5486f6362dc18baa040813093a3b
adamjmcgrath <adam.mcgrath@auth0.com>
2020-03-29 12:01:21 +0100

    Add CSRF

--- 01-Login/routes/index.js
+++ 01-Login/routes/index.js
@@ -1,10 +1,17 @@
 var router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
+var bodyParser = require('body-parser');
+var csrf = require('csurf');
 
-router.get('/', function (req, res, next) {
+// Session key must match the session name of expressoidc
+var csrfProtection = csrf({ sessionKey: 'identity' });
+var parseForm = bodyParser.urlencoded({ extended: false });
+
+router.get('/', csrfProtection, function (req, res, next) {
   res.render('index', { 
     title: 'Auth0 Webapp sample Nodejs',
-    isAuthenticated: req.isAuthenticated() 
+    isAuthenticated: req.isAuthenticated(),
+    csrfToken: req.csrfToken()
   });
 });
 
@@ -13,6 +20,10 @@ router.get('/profile', requiresAuth(), function (req, res, next) {
     userProfile: JSON.stringify(req.openid.user, null, 2),
     title: 'Profile page'
   });
+});
+
+router.post('/process', requiresAuth(), parseForm, csrfProtection, function (req, res) {
+  res.send('data is being processed');
 });
 
 module.exports = router;
